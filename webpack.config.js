ec741d0a637c05e2a872bdaa5e684d3df59ae448
Vu Nguyen <142940347+nguyenlamvu88@users.noreply.github.com>
2024-11-09 13:11:25 -0800

    Update webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -6,7 +6,7 @@ module.exports = {
   output: {
     path: path.resolve(__dirname, 'build'),
     filename: 'bundle.js',
-    publicPath: '/', // Ensure assets are referenced from the root
+    publicPath: '/global_arms_sales_dashboard/', // Ensure assets are referenced from the root
   },
   module: {
     rules: [
